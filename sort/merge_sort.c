7ec0b4e7ebe7855112edc6fc8cdff787f31f10c7
Maria Giovanna <mgiovannagsales@gmail.com>
2025-01-17 13:36:50 -0400

    merge sort added

---
+++ sort/merge_sort.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+void show_arr(int *arr, int len)
+{
+    for (int i = 0; i < len; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void merge(int arr[], int low, int mid, int high)
+{
+    int i = low, j = mid + 1, k = 0;
+    int temp[high - low + 1];
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            temp[k] = arr[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    while (i <= mid)
+    {
+        temp[k] = arr[i];
+        i++;
+        k++;
+    }
+
+    while (j < high)
+    {
+        temp[k] = arr[j];
+        j++;
+        k++;
+    }
+
+    for (int l = 0; l < k; l++)
+    {
+        arr[l + low] = temp[l];
+    }
+}
+
+void merge_sort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid = (low + high) / 2;
+
+        printf("merge da esquerda merge_sort(arr, %d, %d)\n", low, mid);
+        merge_sort(arr, low, mid);
+
+        printf("merge da direita merge_sort(arr, %d, %d)\n", mid+1, high);
+        merge_sort(arr, mid + 1, high);
+
+        printf("merge(arr, %d, %d, %d)\n", low, mid, high);
+        merge(arr, low, mid, high);
+    }
+}
+
+
+
+int main()
+{
+    int numbers[] = {23, 41, 25, 54, 18, 14};
+    int len = sizeof(numbers) / sizeof(numbers[0]);
+
+    merge_sort(numbers, 0, len - 1);
+    show_arr(numbers, len);
+}
